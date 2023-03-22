0a45e6185f04b6adac489f8fe2ec1b00468ff82e
claudiofitipaldi <126166777+claudiofitipaldi@users.noreply.github.com>
2023-03-22 05:58:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botonMostrarMas = document.getElementById('mostrar-mas');
+const informacionAdicional = document.getElementById('informacion-adicional');
+
+botonMostrarMas.addEventListener('click', () => {
+	informacionAdicional.style.display = 'block';
+});
